daa4e5db895e26a0c34638d41c0f51d325919469
kelvin Mutuota <kelvinmutuota749@gmail.com>
2025-09-29 07:56:50 +0300

    fix(app): fix the web configuration output to server

---
+++ app/_layout.native.tsx
@@ -0,0 +1,36 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ElevenLabsProvider } from "@elevenlabs/react-native";
+import { Stack } from "expo-router";
+
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+      <ElevenLabsProvider>
+        <Stack >
+            <Stack.Protected guard={isSignedIn}>
+              <Stack.Screen name="(protected)" options={{ headerShown: false, headerTitle: 'Session' }} />
+            </Stack.Protected>
+          <Stack.Protected guard={!isSignedIn}>
+            <Stack.Screen name="(public)" options={{ headerShown: false }} />
+          </Stack.Protected>
+        </Stack>
+      </ElevenLabsProvider>
+  )
+}
+
+export default function RootLayout() {
+  return (
+      <ClerkProvider 
+        tokenCache={tokenCache}
+        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      >
+        <RootLayoutWithAuth />
+      </ClerkProvider>
+  )
+}
