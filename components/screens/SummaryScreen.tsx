2a34316fced33916c416acbbd633c01080f49cc3
kelvin Mutuota <kelvinmutuota749@gmail.com>
2025-09-29 07:40:46 +0300

    feat(app): get summary from the api

--- components/screens/SummaryScreen.tsx
+++ components/screens/SummaryScreen.tsx
@@ -1,12 +1,25 @@
 import { ConversationResponse } from '@/utils/types'
 import { useLocalSearchParams } from 'expo-router'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView, Text } from 'react-native'
 import Gradient from '../gradient'
 
 export default function SummaryScreen() {
     const { conversationId } = useLocalSearchParams()
     const [ conversation, setConversation ] = useState<ConversationResponse>()
+
+    useEffect(() => {
+        getSummary();
+    }, []);
+
+    async function getSummary() {
+        const response = await fetch(
+            `${process.env.EXPO_PUBLIC_API_URL}/api/conversations?conversationId=${conversationId}`
+        );
+
+        const data: { conversation: ConversationResponse } = await response.json();
+        setConversation(data.conversation);
+    }
 
   return (
     <>
@@ -15,7 +28,7 @@ export default function SummaryScreen() {
         contentInsetAdjustmentBehavior='automatic'
         contentContainerStyle={{ paddingHorizontal: 16 }}
     >
-        <Text>Summary</Text>
+        <Text>{ conversation?.agent_id}</Text>
 
     </ScrollView>
     </>
